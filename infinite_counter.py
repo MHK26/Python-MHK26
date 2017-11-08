2e1506e659283c0e2c45b5b963874ae6131f937a
System Administrator <root@Skynet.local>
2017-11-08 15:43:48 +0100

    new programm

---
+++ infinite_counter.py
@@ -0,0 +1,11 @@
+print ("hi")
+
+
+def counter (start = 0, step = 1):
+	n = start
+	while n >= 0:
+		print (n)
+		n = n + step
+
+counter ()
+
---
+++ intelli.py
@@ -0,0 +1,21 @@
+
+while True:
+	n = raw_input ("Please enter 'hello' :")
+	if n.strip() == 'hello':
+		print ("hello my name is alice" + "   " + "what's your name?")
+
+	y = raw_input ()
+	if y.strip() in ['max' , 'benni']:
+		print ("nice too meet you " + y)
+
+	x = raw_input ("how do feel today?:")
+	if x.strip() == 'fine':
+		print ("good to hear that!")
+	else:
+		print ("why you feeling bad?")
+		
+
+
+
+
+
